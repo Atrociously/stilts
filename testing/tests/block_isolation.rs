3a897bb8e5024b56c9349ce81a1ac51e54c68ccd
Adrian Wannenmacher <tfld@tfld.dev>
2024-10-08 20:23:29 +0200

    improve block isolation tests

--- testing/tests/block_isolation.rs
+++ testing/tests/block_isolation.rs
@@ -4,16 +4,38 @@ use stilts::Template;
 
 #[derive(Template)]
 #[stilts(path = "sample.html", block = "main")]
-struct MyTemplate<'a> {
+struct ChildTemplate<'a> {
     a: &'a str,
 }
 
 #[test]
-fn ensure_matches() {
+fn ensure_child() {
     const EXPECTED: &str = r#"Hello Word<a href="/">MY MAN</a>my code content <a></a>"#;
 
-    let val = MyTemplate {
+    let val = ChildTemplate {
         a: "my code content <a></a>",
+    }
+    .render()
+    .unwrap();
+
+    assert_eq!(val, EXPECTED);
+}
+
+#[derive(Template)]
+#[stilts(
+    content = "<em> {% block main %} <strong> {% a %} </strong> {% end %} </em>",
+    block = "main"
+)]
+struct SoloTemplate<'a> {
+    a: &'a str,
+}
+
+#[test]
+fn ensure_solo() {
+    const EXPECTED: &str = r#"<strong>Hey</strong>"#;
+
+    let val = SoloTemplate {
+        a: "Hey",
     }
     .render()
     .unwrap();
